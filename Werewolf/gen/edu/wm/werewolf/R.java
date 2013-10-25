0bb1b35201e42b9adb5490de18146a7bc2173e1f
jeche <jlchen@email.wm.edu>
2013-10-24 22:58:53 -0400

    moved to scratch

--- Werewolf/gen/edu/wm/werewolf/R.java
+++ Werewolf/gen/edu/wm/werewolf/R.java
@@ -45,13 +45,15 @@ public final class R {
         public static final int verifyPassword=0x7f080009;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_gamestatus=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
+        public static final int activity_gamestatus=0x7f05000b;
         public static final int activity_main=0x7f05000a;
         public static final int app_name=0x7f050000;
         public static final int firstName=0x7f050007;
--- Werewolf/src/edu/wm/werewolf/constants/Constants.java
+++ Werewolf/src/edu/wm/werewolf/constants/Constants.java
@@ -2,7 +2,12 @@ package edu.wm.werewolf.constants;
 
 public class Constants {
 	static private String url = "http://secure-lake-6285.herokuapp.com/";
+	static private String success = "success";
 	public String getBaseUrl(){
 		return url;
+	}
+	
+	public String success(){
+		return success;
 	}
 }
